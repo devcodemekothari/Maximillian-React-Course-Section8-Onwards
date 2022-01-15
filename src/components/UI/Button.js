a80e0f67df38ca0208853cae4822704ccc52f417
Dev Kothari <thisisdevkothari@gmail.com>
2022-01-15 10:34:59 +0530

    Listening to form onChange elements for user input

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -4,8 +4,8 @@ import classes from "./Button.module.css";
 const Button = (props) => {
   return (
     <button
-      onClick={props.onAddUser}
-      type={props.type}
+      onClick={props.onClick}
+      type={props.type || 'button'}
       className={classes.button}
     >
       {props.children}
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 import classes from "./AddUser.module.css";
 
 const AddUser = () => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log("Form Submitted");
+    console.log("Form Submitted with Username: " + enteredUsername + " and Age: " + enteredAge);
+
+
+    // setEnteredAge('');
+    // setEnteredUsername('');
   };
+
+  const usernameChangeHandler = (event) =>{
+    setEnteredUsername(event.target.value);
+  }
+
+  const ageChangeHandler = (event) =>{
+      setEnteredAge(event.target.value);
+  }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" onChange={usernameChangeHandler}></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
-        <Button onAddUser={addUserHandler} type="submit">
+        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <Button onClick={addUserHandler} type="submit">
           Add User
         </Button>
       </form>
