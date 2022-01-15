e5ee7c007567fb054a2bd75ab818b567f0f398eb
Dev Kothari <thisisdevkothari@gmail.com>
2022-01-15 10:55:57 +0530

    Adding static Users List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const users = [
+    {
+      name:'Dev',
+      age: '27'
+    }
+  ];
+
   return (
     <div>
-      <AddUser></AddUser>            
+      <AddUser></AddUser>
+      <UsersList users={users}></UsersList>            
     </div>
   );
 }
